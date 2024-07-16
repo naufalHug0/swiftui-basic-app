38b7d0c085b5be81887e4afae6f065c1832b9206
Naufal Hugo <naufalhugo.a@gmail.com>
2024-07-16 10:13:33 +0700

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by naufal on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
